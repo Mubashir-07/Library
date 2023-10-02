844ad693343e821970a8f45de48d9e9de116942f
Mubashir-07 <mubashirmhd34@protonmail.com>
2023-10-02 22:46:55 +0530

    test

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -14,6 +14,7 @@ class BranchController extends Controller
      */
     public function index(Request $request)
     {
+
         $search = $request['search'] ?? "";
 
         $branches = Branch::when($request->input('search') != '', function ($query) use ($search) {
